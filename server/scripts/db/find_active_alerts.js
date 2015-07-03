3ea471279c145b0ed46e63a590bcfa8b41197ca6
Sonia Fabre <sonia.fabre@gmail.com>
2015-07-03 10:30:43 +0800

    minor change to prevent parsing problems in script

--- server/scripts/db/find_active_alerts.js
+++ server/scripts/db/find_active_alerts.js
@@ -1,5 +1,5 @@
 
 db.alerts.find({active: true}).forEach(function(item) {
-  print(item._id + "\t" + item.name + "\t" +
-    item.code + "\t" + item.email)
+  print(item._id + "\t" + item.email + "\t" +
+    item.code + "\t" + item.name)
 })
